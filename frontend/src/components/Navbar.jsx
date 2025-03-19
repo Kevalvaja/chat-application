3dc0f07651f84d7831380a62b107708a05501d8d
keval vaja <kdvaja4848@gmail.com>
2025-03-19 10:07:20 +0530

    changes by keval 19-03-2025

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { useAuthStore } from '../store/useAuthStore'
 import { Link } from 'react-router-dom'
-import { LogOut, MessageSquare, Settings, User } from 'lucide-react'
+import { LogOut, MessageSquare, Moon, Settings, Sun, User } from 'lucide-react'
+import { useThemeStore } from '../store/useThemeStore'
 
 const Navbar = () => {
   const { logout, authUser } = useAuthStore()
+
+  const { theme, setTheme } = useThemeStore();
+
   return (
     <header className='bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg bg-base-100/80'>
       <div className='container mx-auto px-4 h-16'>
@@ -14,13 +18,19 @@ const Navbar = () => {
               <div className='size-9 rounded-lg bg-primary/10 flex items-center justify-center'>
                 <MessageSquare className='w-5 h-5 text-primary' />
               </div>
-              <h1 className='text-lg font-bold'>Chatty</h1></Link>
+              <h1 className='text-lg font-bold'>Chat Application</h1></Link>
           </div>
           <div className='flex gap-4'>
-            <Link to={"/settings"} className='flex items-center gap-2'>
-              <Settings className='size-4' />
-              <span className='hidden sm:inline'>Settings</span>
-            </Link>
+            <div className='flex items-center gap-2'>
+              <label class="toggle text-base-content">
+                <input type="checkbox" onClick={() => {
+                  theme == "light" ? setTheme("dark") : setTheme("light")
+                }} />
+                <Sun size={'18'} />
+                <Moon size={'18'} />
+              </label>
+              {/* <span className='hidden sm:inline'>Settings</span> */}
+            </div>
             {authUser && (
               <>
                 <Link to={"/profile"} className={`btn btn-sm gap-2`}>
