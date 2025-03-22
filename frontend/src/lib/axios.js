44901fa61c8410c116441fb97ad921d80019a93e
keval vaja <kdvaja4848@gmail.com>
2025-03-22 14:23:32 +0530

    changes by keval 22-03-2025

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 console.log(import.meta.env.NODE)
 
-const LocalURLArr = ["development", undefined, "undefined"]
-export const BASE_URL = LocalURLArr.includes(import.meta.env.NODE) ? "http://localhost:5001" : "/"
-// export const BASE_URL = import.meta.env.NODE === "development" ? "http://localhost:5001" : "/"
+// const LocalURLArr = ["development", undefined, "undefined"]
+// export const BASE_URL = LocalURLArr.includes(import.meta.env.NODE) ? "http://localhost:5001" : "/"
+export const BASE_URL = import.meta.env.NODE === "development" ? "http://localhost:5001" : "/"
 
 export const axiosInstance = axios.create({
-    baseURL: LocalURLArr.includes(import.meta.env.NODE) ? `${BASE_URL}/api` : `${BASE_URL}api`,
-    // baseURL: import.meta.env.NODE === "development" ? `${BASE_URL}/api` : `${BASE_URL}api`,
+    // baseURL: LocalURLArr.includes(import.meta.env.NODE) ? `${BASE_URL}/api` : `${BASE_URL}api`,
+    baseURL: import.meta.env.NODE === "development" ? `${BASE_URL}/api` : `${BASE_URL}api`,
     withCredentials: true,
 })
